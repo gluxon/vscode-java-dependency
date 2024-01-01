dda0f91b57988f58c78115bc208c5a0b33f222df
Brandon Cheng <gluxon@users.noreply.github.com>
2024-01-01 18:49:26 -0500

    Add a few comments around java.import.exclusions settings sharing

--- src/utils/getExclusionGlob.ts
+++ src/utils/getExclusionGlob.ts
@@ -1,13 +1,15 @@
-import { workspace, WorkspaceConfiguration } from "vscode";
-
-export function getJavaConfiguration(): WorkspaceConfiguration {
-	return workspace.getConfiguration('java');
-}
+import { workspace } from "vscode";
 
 // https://github.com/redhat-developer/vscode-java/blob/f598b7938c771987b0da0f85225540ed9af6ffca/src/utils.ts#L100-L112
 export function getExclusionGlob(): string {
-    const config = getJavaConfiguration();
-    const exclusions: string[] = config.get<string[]>("import.exclusions", []);
+    // This setting (java.import.exclusions) is shared with the vscode-java
+    // extension. The schema is defined there rather than in this extension.
+    //
+    // If a user installs this extension (vscode-java-dependency) but not
+    // vscode-java, the java.import.exclusions config in VS Code settings.json
+    // will appear unused.
+    const exclusions: string[] = workspace.getConfiguration('java').get<string[]>("import.exclusions", []);
+
     const patterns: string[] = [];
     for (const exclusion of exclusions) {
         if (exclusion.startsWith("!")) {
